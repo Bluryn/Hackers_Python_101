4e7c2877ecb1a49913994f1068cfba93040f74fc
Bluryn <fallback@email.com>
2024-07-13 15:00:36 +0200

    End Intro + Structure

---
+++ 00_Intro/syntax-demo.py
@@ -0,0 +1,11 @@
+def test():
+    print("test")
+    print("test2")
+    string = "string"
+    number = 10
+#all what is under "def test()" (tab) will be part of the test
+test()
+
+x= 1 + 2 \
+    + 3
+print(x)
