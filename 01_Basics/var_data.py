e09e383940f3bc6b89948350eee04fab4f98e42b
Bluryn <fallback@email.com>
2024-07-14 01:01:14 +0200

    Variables_and_Data_types

---
+++ 01_Basics/var_data.py
@@ -0,0 +1,88 @@
+####################
+#Simple declaration#
+####################
+name = "name" # declaratio of a string
+print(name)
+
+name_length = 4 #declarition of an integer
+print(name_length)
+
+name, name_length = "name" , 4 #In-line declaration of multiple variables
+
+print(type(name)) #Python will try to guess the type (string)
+print(type(name_length)) #Python will try to guess the type (integer)
+
+#################################
+#Specify type within declaration#
+#################################
+name_length = "4" #Here it a string !   
+print(type(name_length)) #will return str
+
+name_length = int("4") #Here it an integer !   
+print(type(name_length)) #will return int
+
+name = int("name") #Here if we print we will get an error
+
+##################
+#Case sensitivity#
+##################
+
+name_length = 4
+Name_length = 5
+# if we print those variables it will return 4 and 5 -> not the same !!!
+
+##################
+#List declaration#
+##################
+
+name_list = ["name", "oui", "non"]
+print(type(name_list)) # will return list
+
+name1, name2, name3 = name_list #It will assign in order the values in the list for each variable
+#we can print them separately
+print(name1)
+print(name2)
+print(name3)
+
+###################
+#Tuple declaration#
+###################
+
+name_tuple = ("name", "oui", "non")
+print(type(name_tuple)) #will print tuple
+
+###################
+#Tuple dictionnary#
+###################
+
+name_dictionnary = {"name", "oui", "non"}
+print(type(name_dictionnary)) #will print dictionnary
+
+###############
+#Tuple boolean#
+###############
+
+name_boolean = True
+print(type(name_boolean)) #will print boolean
+
+#############
+#Tuple range#
+#############
+
+name_range = range(6)
+print(type(name_range)) #will print range
+
+#############
+#Tuple bytes#
+#############
+
+name_bytes = b"name"
+print(type(name_bytes)) #will print bytes
+
+print(name_tuple)       # ("name", "oui", "non")
+print(name_list)        # ["name", "oui", "non"]
+print(name_dictionnary) # {"name", "oui", "non"}
+print(name_boolean)     # True
+print(name_range)       # range(0,6)
+print(name_bytes)       # b'name'
+
