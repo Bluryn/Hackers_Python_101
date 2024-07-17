3921d2263c76f663493bf090b86247f7fa1823a9
Bluryn <fallback@email.com>
2024-07-17 08:13:29 +0200

    String - completion of file

--- 01_Basics/02_strings.py
+++ 01_Basics/02_strings.py
@@ -19,6 +19,24 @@ print(String5)
 String6 = "I\"m a string\n I\"m on a newline =)"
 Print(String6)
 
-String7 = "a" + 10
-
+String7 = "a" * 10
+#Python is capable of multiplying strings
 Print(String7)
+
+#We can also enumrate the number of characters
+print(len(String7))
+
+#We can also check if certain charcaters are ine the String
+print("new" in String4) # Will returne False
+
+#We can return if a String begins with a precise caracters
+print(String6.startswith("L")) #Will return false
+print(String6.startswith("I")) #Will return true
+
+#We can also play with indexes for example when a characters appears in the string
+print(String5.index("string")) #will return 6
+
+#We can play with case
+print(String5.upper())
+print(String5.lower())
+
