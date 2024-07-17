dd243395c96ec966d6955212b1f1b77a55433309
Bluryn <fallback@email.com>
2024-07-17 08:04:10 +0200

    Strings - first push

---
+++ 01_Basics/02_strings.py
@@ -0,0 +1,24 @@
+String1 = "String =)"
+String2 = 'Also string =)'
+
+print(String1)
+print(String2)
+
+String3 = """Very 
+Long 
+String =)"""
+
+print(String3)
+
+String4 = "I\"m a string =)"
+print(String4)
+
+String5 = 'I\'m a string =)'
+print(String5)
+
+String6 = "I\"m a string\n I\"m on a newline =)"
+Print(String6)
+
+String7 = "a" + 10
+
+Print(String7)
