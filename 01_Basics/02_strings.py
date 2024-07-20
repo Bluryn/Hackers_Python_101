688ad52f238f5413dc624709b5bf4bea562bc584
Bluryn <fallback@email.com>
2024-07-20 22:09:10 +0200

    Strings

--- 01_Basics/02_strings.py
+++ 01_Basics/02_strings.py
@@ -45,3 +45,41 @@ bordel_string= "    Messy String =( "
 print(bordel_string)
 #We have a function to clean the additionnal spaces in the string
 print(bordel_string.strip())
+
+#We can also replace a character within a string
+print(bordel_string.replace("=(","=)").strip()) # We are also stripping
+
+#We can split a string and specifiy a seperator
+print(bordel_string.split()) #will find every words
+print(bordel_string.split(",")) #will find every character chain between a ,
+      
+#New clean example
+stringx = "new clean string ! Yeah"
+
+#Here are some enteresting functions - encoding (used for exploits)
+print(stringx.encode())
+print(stringx.encode("utf-8")) #specified encoding
+
+#We can play with alignement
+print(stringx.rjust(25)) #it will add spaces so the string is 25 characters
+print(stringx.rjust(25,"Y")) #spaces a replaces with Ys
+      
+#Concatenation
+print("New" + "clean string ! Yeaj")
+print("stringx" + str(len(stringx)) + " characters long")
+
+print(1 + 1)
+print("1" + "1")
+print(type("1" + "1"))
+
+#Here is an example of formating 
+print("stringx is {} characters long!".format(len(stringx))) # {} is the place holder
+print("{} {} {}".format(len(stringx), 4.0, 0x12)) #we can have multiple format place holder and print other variables
+print("{0} {2} {1}".format)(len(stringx), 4.0, 0x12) # in this case we are specifying index for placeholders so that the order change
+print("{length}".format(length=len(stringx)))# we can put a variable in the place holder
+      
+
+length = len(stringx)
+print(f"stringx is {length} characters long") #other way of printing format
+print("stringx is {length:.2f} characters long") #will output the lenght in float with 2 digit after decimal
+# we can also print in binary, octo and hexidical ({length:b}, {length:x}, lenghth{o})
